37def8f1ad71c84a8903ae7ef907de1c42f1d907
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-05-28 01:42:39 +0200

    Add files via upload

--- Javascript/my-app.js
+++ Javascript/my-app.js
@@ -2,18 +2,27 @@
 var myApp = new Framework7();
 // Export selectors engine
 var $$ = Dom7;
-// Add view
-//var mainView = myApp.addView('.view-main', {
-// Because we use fixed-through navbar we can enable dynamic navbar
-//  dynamicNavbar: true
-//});
+//Add view
+var mainView = myApp.addView('.view-main', {
+        domCache: true
+    });
+
+
 myApp.onPageInit('index', function (page) {
+    "use strict";
     var pageContainer = $$(page.container);
     pageContainer.find('.list-button').on('click', function () {
-        //var username = pageContainer.find('input[name="username"]').val();
-        //var password = pageContainer.find('input[name="password"]').val();
+        var username = pageContainer.find('input[name="username"]').val();
+        var password = pageContainer.find('input[name="password"]').val();
         // Handle username and password
-        myApp.alert('Username: ' + username + ', Password: ' + password);
+        if (username === "test" && password === "test") {
+            myApp.alert("ingelogd");
+            mainView.router.loadPage("#about");
+            
+        } else {
+            myApp.alert("verkeerde inloggegevens");
+        }
+        
     });
 });
 myApp.init();
